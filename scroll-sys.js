aa104cf19bdc6688093debc867a6aa626ef0534f
Your <takumikun0730@outlook.jp>
2022-06-16 21:41:14 +0200

    v beta 0.0.2 initialization ablum example added edited ablum hash for scroll

---
+++ scroll-sys.js
@@ -0,0 +1,14 @@
+function scrollCode(element) {
+    var bodyRect = document.body.getBoundingClientRect(),
+        elemRect = document.getElementById(element).getBoundingClientRect(),
+        offset   = elemRect.top - bodyRect.top;
+
+    window.scrollTo(0, offset);
+}
+
+function getHash() {
+    var hash = window.location.hash.substr(1);
+    return hash
+}
+
+scrollCode(getHash());
